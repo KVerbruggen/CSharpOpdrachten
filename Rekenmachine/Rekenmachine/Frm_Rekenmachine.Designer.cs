40400d29c6071b51ad6fef121f9d2ae59423ef12
Kevin Verbruggen <Kevinverbruggen@live.nl>
2017-10-31 11:19:01 +0100

    Basic UI added

---
+++ Rekenmachine/Rekenmachine/Frm_Rekenmachine.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Rekenmachine
+{
+    partial class Frm_Rekenmachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbCalculation = new System.Windows.Forms.TextBox();
+            this.btClear = new System.Windows.Forms.Button();
+            this.btClearEntry = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.btAdd = new System.Windows.Forms.Button();
+            this.btSubtract = new System.Windows.Forms.Button();
+            this.btMultiply = new System.Windows.Forms.Button();
+            this.btDivide = new System.Windows.Forms.Button();
+            this.btSubmit = new System.Windows.Forms.Button();
+            this.btBackspace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbCalculation
+            // 
+            this.tbCalculation.Location = new System.Drawing.Point(13, 13);
+            this.tbCalculation.Name = "tbCalculation";
+            this.tbCalculation.Size = new System.Drawing.Size(138, 20);
+            this.tbCalculation.TabIndex = 0;
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(13, 39);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(30, 30);
+            this.btClear.TabIndex = 1;
+            this.btClear.Text = "C";
+            this.btClear.UseVisualStyleBackColor = true;
+            // 
+            // btClearEntry
+            // 
+            this.btClearEntry.Location = new System.Drawing.Point(49, 39);
+            this.btClearEntry.Name = "btClearEntry";
+            this.btClearEntry.Size = new System.Drawing.Size(30, 30);
+            this.btClearEntry.TabIndex = 2;
+            this.btClearEntry.Text = "CE";
+            this.btClearEntry.UseVisualStyleBackColor = true;
+            // 
+            // bt0
+            // 
+            this.bt0.Location = new System.Drawing.Point(49, 184);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(30, 30);
+            this.bt0.TabIndex = 3;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = true;
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(13, 148);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(30, 30);
+            this.bt1.TabIndex = 4;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = true;
+            // 
+            // bt2
+            // 
+            this.bt2.Location = new System.Drawing.Point(49, 148);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(30, 30);
+            this.bt2.TabIndex = 5;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = true;
+            // 
+            // bt3
+            // 
+            this.bt3.Location = new System.Drawing.Point(85, 148);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(30, 30);
+            this.bt3.TabIndex = 6;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = true;
+            // 
+            // bt4
+            // 
+            this.bt4.Location = new System.Drawing.Point(13, 111);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(30, 30);
+            this.bt4.TabIndex = 7;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = true;
+            // 
+            // bt5
+            // 
+            this.bt5.Location = new System.Drawing.Point(49, 111);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(30, 30);
+            this.bt5.TabIndex = 8;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = true;
+            // 
+            // bt6
+            // 
+            this.bt6.Location = new System.Drawing.Point(85, 111);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(30, 30);
+            this.bt6.TabIndex = 9;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = true;
+            // 
+            // bt7
+            // 
+            this.bt7.Location = new System.Drawing.Point(13, 75);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(30, 30);
+            this.bt7.TabIndex = 10;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = true;
+            // 
+            // bt8
+            // 
+            this.bt8.Location = new System.Drawing.Point(49, 75);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(30, 30);
+            this.bt8.TabIndex = 11;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = true;
+            // 
+            // bt9
+            // 
+            this.bt9.Location = new System.Drawing.Point(85, 75);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(30, 30);
+            this.bt9.TabIndex = 12;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = true;
+            // 
+            // btAdd
+            // 
+            this.btAdd.Location = new System.Drawing.Point(121, 39);
+            this.btAdd.Name = "btAdd";
+            this.btAdd.Size = new System.Drawing.Size(30, 30);
+            this.btAdd.TabIndex = 13;
+            this.btAdd.Text = "+";
+            this.btAdd.UseVisualStyleBackColor = true;
+            // 
+            // btSubtract
+            // 
+            this.btSubtract.Location = new System.Drawing.Point(121, 75);
+            this.btSubtract.Name = "btSubtract";
+            this.btSubtract.Size = new System.Drawing.Size(30, 30);
+            this.btSubtract.TabIndex = 14;
+            this.btSubtract.Text = "-";
+            this.btSubtract.UseVisualStyleBackColor = true;
+            // 
+            // btMultiply
+            // 
+            this.btMultiply.Location = new System.Drawing.Point(121, 111);
+            this.btMultiply.Name = "btMultiply";
+            this.btMultiply.Size = new System.Drawing.Size(30, 30);
+            this.btMultiply.TabIndex = 15;
+            this.btMultiply.Text = "×";
+            this.btMultiply.UseVisualStyleBackColor = true;
+            // 
+            // btDivide
+            // 
+            this.btDivide.Location = new System.Drawing.Point(121, 148);
+            this.btDivide.Name = "btDivide";
+            this.btDivide.Size = new System.Drawing.Size(30, 30);
+            this.btDivide.TabIndex = 16;
+            this.btDivide.Text = "÷";
+            this.btDivide.UseVisualStyleBackColor = true;
+            // 
+            // btSubmit
+            // 
+            this.btSubmit.Location = new System.Drawing.Point(121, 184);
+            this.btSubmit.Name = "btSubmit";
+            this.btSubmit.Size = new System.Drawing.Size(30, 30);
+            this.btSubmit.TabIndex = 17;
+            this.btSubmit.Text = "=";
+            this.btSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btBackspace
+            // 
+            this.btBackspace.Location = new System.Drawing.Point(85, 39);
+            this.btBackspace.Name = "btBackspace";
+            this.btBackspace.Size = new System.Drawing.Size(30, 30);
+            this.btBackspace.TabIndex = 18;
+            this.btBackspace.Text = "←";
+            this.btBackspace.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Rekenmachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(162, 219);
+            this.Controls.Add(this.btBackspace);
+            this.Controls.Add(this.btSubmit);
+            this.Controls.Add(this.btDivide);
+            this.Controls.Add(this.btMultiply);
+            this.Controls.Add(this.btSubtract);
+            this.Controls.Add(this.btAdd);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.btClearEntry);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.tbCalculation);
+            this.Name = "Frm_Rekenmachine";
+            this.Text = "Rekenmachine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCalculation;
+        private System.Windows.Forms.Button btClear;
+        private System.Windows.Forms.Button btClearEntry;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button btAdd;
+        private System.Windows.Forms.Button btSubtract;
+        private System.Windows.Forms.Button btMultiply;
+        private System.Windows.Forms.Button btDivide;
+        private System.Windows.Forms.Button btSubmit;
+        private System.Windows.Forms.Button btBackspace;
+    }
+}
+
